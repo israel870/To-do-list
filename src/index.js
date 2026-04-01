0ca5c8c7a73ec8ca8c261adc7c502c63bf7d5137
unknown <israeltobiloba24@gmail.com>
2026-04-01 11:54:00 +0100

    Comment

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// let myRadio = document.getElementById('radioCheck');
+// console.log(myRadio);
+// if(myRadio.checked = true){
+//     alert("Checked")
+// } else {
+//     alert("not checked")
+// }
+
