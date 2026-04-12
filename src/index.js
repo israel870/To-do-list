abd98adcd7382941405ea928c4bd021652448157
unknown <israeltobiloba24@gmail.com>
2026-04-12 13:51:25 +0100

    second

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@
 // } else {
 //     alert("not checked")
 // }
-
+const newTask = document.getElementById('newTask');
+const modalBg = document.getElementById('modalBg');
+newTask.addEventListener('click', () => {
+    modalBg.classList.remove('hidden');
+    modalBg.classList.add('flex')
+});
+let listInput = document.getElementById('listInput');
+let addTask = document.getElementById('addTask');
+addTask.addEventListener('click', () => {
+    if (listInput.value = "" ) {
+        alert('Please input something');
+        return;
+    }
+})
